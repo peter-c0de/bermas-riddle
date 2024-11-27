29325bd3e32a37dbc916d67d6a4168832c3dc484
Peter c0de <pete_coder@hotmail.com>
2024-11-27 11:35:49 +0200

    Added: code_adv.py Removed Code: and (c%7==0)

---
+++ code_adv.py
@@ -0,0 +1,8 @@
+def berma_eggs():
+    # From 7 to 3493:
+    for i in range(1, 500):
+        c = 7 * i # N7 Multiplications
+        if (c%4==1) and (c%5==1) and (c%6==1):
+            print(c)
+
+berma_eggs()
